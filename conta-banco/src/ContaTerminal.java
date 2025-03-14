e9d110cc32d600a616cd188991fa5296ba19c98b
Jonny Paiva <jonny_paiva@hotmail.com.br>
2025-03-14 09:49:41 -0300

    commit

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    String agencia = "067-8";
+    int conta = 1021;
+
+    System.out.println("Seja bem Vindo ao Banco Bradesco!");
+    System.out.println("Vamos abrir a sua conta");
+    System.out.println("Para iniciarmos, me informe o seu NOME: ");
+        String nome = scanner.next();
+    System.out.println("Olá " + nome + ", me informa agora, seu SOBRENOME: ");
+        String sobrenome = scanner.next();
+    System.out.println("Obrigado por informar!");
+    System.out.println("Vamos agora, realizar o seu primeiro depósito.");
+    System.out.println("Informe a quantia a ser depositada:");
+        double saldo = scanner.nextDouble();
+    System.out.println("Estou processando seu depósito.");
+    System.out.println("Tudo certo!!");
+
+
+    System.out.println("Olá, " + nome + " " + sobrenome + ", Obrigado por criar uma conta em nosso banco.");
+    System.out.println("Sua Agência é: " + agencia + ", sua conta é: " + conta + ", e seu saldo de " + saldo + " já está disponível para saque.");
+
     }
 }
